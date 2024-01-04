06d8d1a0b5f27c12e9f469fc661cebaf52a624b3
RADHIKA R <143479395+uniqueradhi@users.noreply.github.com>
2024-01-04 17:23:51 +0530

    Add files via upload
    MILESTONE-6

---
+++ ExecutionContext&scope.js
@@ -0,0 +1,15 @@
+/*There are two types of execution context in js
+    1.global execution.
+    2.function execution.
+FUNCTION EXECUTION:
+        The global execution context is created when a JavaScript script first starts to run, 
+ and it represents the global scope in JavaScript.
+GLOBAL EXECUTION:
+     A function execution context is created whenever a function is called, representing the 
+ function's local scope.
+
+ How execution phase id created?
+    1.creation phase.
+    2.execution phase.
+
+*/ 
